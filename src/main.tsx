20bc6e1ec94d7433e069643c0867ff38dfe1ef25
마조리카 <neu5563@naver.com>
2026-05-22 15:29:32 +0900

    chore: install vapor ui and tailwind dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from '@vapor-ui/core';
 import './index.css';
 import App from './app/App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="light">
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
